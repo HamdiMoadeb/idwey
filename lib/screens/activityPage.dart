a1a70183a3614ddb10d01eb8ddfb5f1a435a79b1
vergo789 <ahmedachour789@gmail.com>
2023-01-28 20:13:09 +0100

    feature: create activities page

---
+++ lib/screens/activityPage.dart
@@ -0,0 +1,157 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:idwey/utils/colors.dart';
+import 'package:idwey/widgets/common/scaffold.dart';
+import '../widgets/common/footer.dart';
+
+import '../models/activity.dart';
+import '../services/activityCalls.dart';
+import '../widgets/listItems/activityListItem.dart';
+import '../widgets/tabs/ActivityFilterTab.dart';
+
+class ActivityPage extends StatefulWidget {
+  const ActivityPage({Key? key}) : super(key: key);
+
+  @override
+  State<ActivityPage> createState() => _ActivityPageState();
+}
+
+class _ActivityPageState extends State<ActivityPage> {
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
+  final scrollController = ScrollController();
+  dynamic searchInputs = {'start': '', 'end': '', 'address': '', 'adults': ''};
+
+  void scrollToTop() {
+    scrollController.animateTo(0,
+        duration: const Duration(seconds: 2), curve: Curves.linear);
+  }
+
+  void updateSearchFields(dynamic searchInputs) {
+    setState(() {
+      this.searchInputs = searchInputs;
+    });
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(
+        SystemUiOverlayStyle(statusBarColor: primaryGrey));
+    return CommonScaffold(
+      scaffoldKey: _scaffoldKey,
+      body: SingleChildScrollView(
+        controller: scrollController,
+        child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Container(
+                child: Stack(
+                  children: [
+                    Stack(
+                      children: [
+                        Container(
+                          height: 230,
+                          child: SizedBox(
+                            width: MediaQuery.of(context).size.width,
+                            child: Image.asset("assets/activitycover.jpg",
+                                fit: BoxFit.cover),
+                          ),
+                        ),
+                        const Positioned.fill(
+                          child: Align(
+                            alignment: Alignment.center,
+                            child: Text(
+                              'Ateliers et activités',
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 28,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Container(
+                      margin: const EdgeInsets.only(top: 180),
+                      child: ActivityFilterTab(
+                        onChangeField: (dynamic searchInputs) =>
+                            updateSearchFields(searchInputs),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              ActivityList(searchInputs: searchInputs),
+              //footer
+              Footer(),
+              CreatedBy(),
+              BackToTop(scrollToTop),
+            ]),
+      ),
+    );
+  }
+}
+
+class ActivityList extends StatefulWidget {
+  dynamic searchInputs;
+  ActivityList({Key? key, required this.searchInputs}) : super(key: key);
+
+  @override
+  State<ActivityList> createState() => _ActivityListState();
+}
+
+class _ActivityListState extends State<ActivityList> {
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: ActivityCalls.getActivityList(widget.searchInputs),
+      builder: (context, AsyncSnapshot<List<Activity>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done &&
+            snapshot.data != null) {
+          final List<Activity> listActivities = snapshot.data!.toList();
+
+          if (listActivities != null) {
+            return Column(
+              children: [
+                Container(
+                  padding: EdgeInsets.all(20),
+                  child: Text(
+                    "${listActivities.length} hébergements trouvés",
+                    style: TextStyle(
+                      fontSize: 24.0,
+                      color: titleBlue,
+                    ),
+                  ),
+                ),
+                ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemBuilder: (BuildContext context, int index) => Container(
+                      margin: EdgeInsets.only(bottom: 15, right: 15),
+                      child: ActivityListItem(listActivities[index])),
+                  itemCount: listActivities.length,
+                ),
+              ],
+            );
+          }
+        }
+
+        return Container(
+          margin: EdgeInsets.only(top: 30),
+          child: Center(
+            child: CircularProgressIndicator(
+              valueColor: AlwaysStoppedAnimation<Color>(primary),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
