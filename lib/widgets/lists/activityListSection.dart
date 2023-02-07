84509796a62d5ec1dc647e931be2e986f5a5d37f
HamdiMoadeb <hamdi.moadeb@tayara.tn>
2023-02-07 11:30:42 +0100

    IDWEYAPP-21 - fix price margin on activity card

--- lib/widgets/lists/activityListSection.dart
+++ lib/widgets/lists/activityListSection.dart
@@ -136,7 +136,7 @@ class _ActivityListSectionState extends State<ActivityListSection> {
           ),
         ),
         Container(
-          height: 420,
+          height: 430,
           margin: EdgeInsets.only(top: 5, bottom: 20, right: 15),
           child: ListView.builder(
             controller: controller,
