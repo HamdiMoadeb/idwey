addfd21c7917d9f3f16f59c58bba222a68cbb3d8
vergo789 <ahmedachour789@gmail.com>
2023-02-27 10:41:59 +0100

    feature: create blog cards and blog api service to show the list

--- lib/screens/blogPage.dart
+++ lib/screens/blogPage.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:idwey/screens/homePage.dart';
+import 'package:idwey/services/blogCalls.dart';
+import 'package:idwey/widgets/listItems/blogListItem.dart';
 
+import '../models/blog.dart';
 import '../utils/colors.dart';
+import '../utils/utils.dart';
+import '../widgets/common/footer.dart';
 import '../widgets/common/scaffold.dart';
 
 class BlogPage extends StatefulWidget {
-  int? id;
-  BlogPage({Key? key, required this.id}) : super(key: key);
+  const BlogPage({
+    Key? key,
+  }) : super(key: key);
 
   @override
   State<BlogPage> createState() => _BlogPageState();
@@ -16,11 +23,34 @@ class _BlogPageState extends State<BlogPage>
     with SingleTickerProviderStateMixin {
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey<ScaffoldState>();
   final scrollController = ScrollController();
+  List<Blog> listBlogs = [];
+  bool loading = false;
   bool showFAB = false;
+
+  callBlogs() {
+    setState(() {
+      loading = true;
+    });
+    BlogCalls.getAllBlogs().then((result) async {
+      setState(() {
+        listBlogs = result;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      setState(() {
+        loading = false;
+      });
+    });
+  }
 
   void scrollToTop() {
     scrollController.animateTo(0,
         duration: const Duration(seconds: 2), curve: Curves.linear);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    checkInternetConnectivity(context, callBlogs);
   }
 
   @override
@@ -36,44 +66,123 @@ class _BlogPageState extends State<BlogPage>
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Container(
-              child: Stack(
-                children: [
-                  Stack(
+            loading
+                ? Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: Center(
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(primary),
+                      ),
+                    ),
+                  )
+                : Column(
                     children: [
                       Container(
-                        height: 230,
-                        child: SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          child: Image.asset("assets/blogCover.jpg",
-                              fit: BoxFit.cover),
+                        child: Stack(
+                          children: [
+                            Stack(
+                              children: [
+                                Container(
+                                  height: 230,
+                                  child: SizedBox(
+                                    width: MediaQuery.of(context).size.width,
+                                    child: Image.asset("assets/blogCover.jpg",
+                                        fit: BoxFit.cover),
+                                  ),
+                                ),
+                                Positioned.fill(
+                                  child: Container(
+                                    child: Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: [
+                                        Text(
+                                          'Blog',
+                                          textAlign: TextAlign.center,
+                                          style: TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 28,
+                                            fontWeight: FontWeight.w500,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
                         ),
                       ),
-                      Positioned.fill(
-                        child: Container(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'Blog',
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 28,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                              ),
-                            ],
-                          ),
+                      Container(
+                        padding:
+                            EdgeInsets.only(top: 15.0, left: 15, right: 15),
+                        child: Row(
+                          children: [
+                            GestureDetector(
+                                onTap: () {
+                                  Navigator.pushReplacement(
+                                      context,
+                                      MaterialPageRoute(
+                                        builder: (context) => HomePage(),
+                                      ));
+                                },
+                                child: NavText("Accueil")),
+                            Container(
+                              width: 8,
+                              height: 8,
+                              margin: EdgeInsets.symmetric(horizontal: 15),
+                              decoration: BoxDecoration(
+                                  color: primaryGrey,
+                                  borderRadius: BorderRadius.circular(10)),
+                            ),
+                            NavText("Blog")
+                          ],
                         ),
+                      ),
+                      Container(
+                          margin: EdgeInsets.only(top: 10, bottom: 20),
+                          child: const Divider(thickness: 1)),
+                      Column(
+                        children: [
+                          ListView.builder(
+                            shrinkWrap: true,
+                            physics: NeverScrollableScrollPhysics(),
+                            itemBuilder: (BuildContext context, int index) =>
+                                Container(
+                                    margin:
+                                        EdgeInsets.only(bottom: 40, right: 15),
+                                    child: BlogPageItems(
+                                      blog: listBlogs[index],
+                                    )),
+                            itemCount: listBlogs.length,
+                          ),
+                        ],
                       ),
                     ],
                   ),
-                ],
-              ),
-            ),
+            Footer(),
+            CreatedBy(),
+            BackToTop(scrollToTop),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class NavText extends StatelessWidget {
+  String title;
+  NavText(this.title);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(),
+      child: Text(
+        title,
+        style: TextStyle(
+            color: primary, fontWeight: FontWeight.w600, fontSize: 14),
       ),
     );
   }
