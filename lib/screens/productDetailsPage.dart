e5a7d30a025f7b67b77653ae9c9307ca1325b13c
vergo789 <ahmedachour789@gmail.com>
2023-03-10 16:41:51 +0100

    feature: init product page

---
+++ lib/screens/productDetailsPage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class ProductDetailsPage extends StatefulWidget {
+  int id;
+  ProductDetailsPage({Key? key, required this.id}) : super(key: key);
+
+  @override
+  State<ProductDetailsPage> createState() => _ProductDetailsPageState();
+}
+
+class _ProductDetailsPageState extends State<ProductDetailsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
