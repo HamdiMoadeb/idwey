bc99761fbc2e14eeb2c47d3b2d43e3ee2a3942a8
vergo789 <ahmedachour789@gmail.com>
2023-03-14 10:01:36 +0100

    feature: call the event detail api

--- lib/services/eventCalls.dart
+++ lib/services/eventCalls.dart
@@ -59,21 +59,18 @@ class EventCalls {
 
   //api for  event details
   static Future<EventDetails> getEventDetails(int id) async {
-    EventDetails eventDetail;
+    EventDetails eventDetail =
+        EventDetails(0, '', '', '', '', 0, '', 0, '', '', '', '', '');
 
     var url = Uri.parse('${Urls.URL_API}event/detail/${id}');
     print(url);
     var response = await http.get(url);
     print('Response status: ${response.statusCode}');
 
-
     if (response.statusCode == 200) {
       final data = jsonDecode(response.body);
       eventDetail = EventDetails.fromJson(data);
-      }
-
     }
-
     return eventDetail;
   }
 }
