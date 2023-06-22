e0fbfdc4ef2c5e9ab01c3ac9a0d35339ed29c129
HamdiMoadeb <hamdi.moadeb@tayara.tn>
2023-06-22 18:19:16 +0100

    150 - login workflow

--- lib/services/authCalls.dart
+++ lib/services/authCalls.dart
@@ -25,4 +25,22 @@ class AuthCalls {
 
     return response;
   }
+
+  static Future<http.Response> login(String email, String password) async {
+    var body = {
+      "email": email,
+      "password": password,
+    };
+
+    String bodyEncoded = jsonEncode(body);
+
+    var url = Uri.parse('${Urls.URL_API}login');
+    var response = await http.post(url,
+        body: bodyEncoded, headers: {"Content-Type": "application/json"});
+
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    return response;
+  }
 }
