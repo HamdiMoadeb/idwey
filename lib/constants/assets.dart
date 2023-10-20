220b17f122844552734d50719d35121839dee3ef
HamdiMoadeb <hamdimoadeb@gmail.com>
2023-10-20 18:57:59 +0100

    replace Insurance icon by fidelity program icon

--- lib/constants/assets.dart
+++ lib/constants/assets.dart
@@ -35,4 +35,5 @@ class Assets {
   static const String frame = "assets/Frame.svg";
   static const String frame2 = "assets/final_sign_up.svg";
   static const String tinyHome = "assets/Tiny home.svg";
+  static const String fidelity = "assets/gift.svg";
 }
